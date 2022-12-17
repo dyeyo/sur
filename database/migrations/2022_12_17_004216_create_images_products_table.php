d80ec4a2d09a938592c7e9d97ff8be72307ea47d
Diego <dyegovallejob@gmail.com>
2022-12-16 19:51:16 -0500

    Feat migraciones and models

---
+++ database/migrations/2022_12_17_004216_create_images_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('img');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images_products');
+    }
+}
